c5b313658e64a83af04f905a8d6a71dcbd4a9d94
sandarshnaroju <72196193+sandarshnaroju@users.noreply.github.com>
2024-03-15 14:04:41 +0530

    Update FileUpload-Downloader.user.js

--- File-upload-bypasser/FileUpload-Downloader.user.js
+++ File-upload-bypasser/FileUpload-Downloader.user.js
@@ -4,8 +4,9 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.file-upload.com/*
-// @include      file-upload
+// @match        https://www.file-upload.org/*
+
+// @include      https://docs.google.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=file-upload.com
 // @grant        GM_openInTab
 // ==/UserScript==
@@ -13,7 +14,16 @@
 (function() {
     'use strict';
     const iframeElements = document.getElementsByTagName("iframe");
-    const srcLink = iframeElements[0].attributes[0].nodeValue;
+
+
+    const requiredIframObject= Object.values(iframeElements).find((iframObj)=>{
+        return iframObj.attributes[0].nodeValue.includes('.pdf');
+    });
+
+
+    const srcLink = requiredIframObject.attributes[0].nodeValue;
+
+
     const googleLink = srcLink.slice(srcLink.indexOf("https://f"),srcLink.indexOf(".pdf")+4);
     console.log("File link", googleLink);
     GM_openInTab(googleLink);
