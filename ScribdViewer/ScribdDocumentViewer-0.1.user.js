952aebb812d4610d12ddeb0f9c1aaa7b787edad3
sandarshnaroju <72196193+sandarshnaroju@users.noreply.github.com>
2024-10-28 12:39:06 +0530

    Create ScribdDocumentViewer-0.1.user.js

---
+++ ScribdViewer/ScribdDocumentViewer-0.1.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         ScribdDocumentViewer
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.scribd.com/document/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=scribd.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const currentUrl = window.location.href;
+   // console.log('Current URL', currentUrl)
+    if(currentUrl.includes("https://www.scribd.com/document/")){
+        const documentId = currentUrl.slice(32, 41)
+        //   console.log('documentId',documentId)
+        const newUrl = 'https://www.scribd.com/embeds/ '+ documentId + '/content'
+        window.open(newUrl,"_top");
+    }
+})();
